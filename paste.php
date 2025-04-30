18a0e2d17a47f5868c022c38637a06b068a9778b
doxsite2 <dozgroxdozgrox@gmail.com>
2025-04-30 10:30:01 +0800

    Update paste.php

--- paste.php
+++ paste.php
@@ -1,17 +1,16 @@
 <?php
 $api_dev_key = '5supLjKUBT9hGpupwPuIAlqppkIx800X';
-$api_paste_code = $_POST['text'] ?? 'Empty';
+$api_paste_code = $_POST['text'] ?? '';
 $api_paste_name = $_POST['title'] ?? 'Untitled';
-$api_paste_private = '1'; // 0=public 1=unlisted 2=private
-$api_paste_expire_date = '10M';
+$api_paste_private = '1'; // 0=public, 1=unlisted
+$api_paste_expire_date = 'N'; // Never expire
 $api_paste_format = 'text';
-$api_user_key = ''; // optional
+$api_user_key = ''; // leave blank for guest paste
 
 $api_paste_name = urlencode($api_paste_name);
 $api_paste_code = urlencode($api_paste_code);
 
 $url = 'https://pastebin.com/api/api_post.php';
-
 $data = 'api_option=paste'
       . '&api_user_key=' . $api_user_key
       . '&api_paste_private=' . $api_paste_private
@@ -25,7 +24,6 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 $response = curl_exec($ch);
 curl_close($ch);
 
